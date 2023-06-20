5372bbcdcef05a2d7448db6b0b856c6aebe96051
M1gguel <134648203+M1gguel@users.noreply.github.com>
2023-06-20 15:06:52 -0300

    Atividade C#

---
+++ 4 números e calcular/4 números e calcular/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_números_e_calcular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota1, nota2, nota3, nota4, total;
+
+            Console.WriteLine("=___________________________=");
+            Console.WriteLine("+         Calcular          +");
+            Console.WriteLine("+           Média           +");
+            Console.WriteLine("=___________________________=");
+
+            Console.Write("Digite o número 1: ");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o número 2: ");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o número 3: ");
+            nota3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o número 4: ");
+            nota4 = Convert.ToInt32(Console.ReadLine());
+
+            total = (nota1 + nota2 + nota3 + nota4) / 4;
+            Console.WriteLine("A média desses número é: " + total);
+
+            Console.WriteLine("=___________________________=");
+            Console.WriteLine("+           Fim             +");
+            Console.WriteLine("=___________________________=");
+        }
+    }
+}
---
+++ caluclo de média se passou ou não/caluclo de média se passou ou não/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caluclo_de_média_se_passou_ou_não
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, nota4, total;
+
+            Console.Write("Digite o número 1: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o número 2: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o número 3: ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o número 4: ");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+
+            total = (nota1 + nota2 + nota3 + nota4) / 4;
+            Console.WriteLine("A média desses número é: " + total);
+
+           if (total >= 7) {
+                Console.WriteLine("Você está aprovado, parabèns!!!");
+            }
+              else {
+                Console.WriteLine("Você está reprovado, talvez dê certo na próxima :(");
+               }
+        }
+    }
+}
