5c65c348d4e4b1291f0985321011041813164b85
M1gguel <134648203+M1gguel@users.noreply.github.com>
2023-07-05 17:24:19 -0300

    update 5.0

---
+++ BibliotecaDeClasses/BibliotecaDeClasses/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClasses
+{
+    internal class Cliente
+    {
+        protected Cliente() {
+       private int IdCliente = 1;
+        public string NomeCompleto = "Miguel Perez Silva";
+        public string Email = "miguelpsilva0@gmail.com";
+        public int Telefone = 199552469;
+        private string Senha = "miguelbrabo";
+        public string Endereco = "Rua Ipanema n:47";
+        public int DataDeNascimento = 04 / 07 / 2005;
+    }
+        
+    }
+
+}
---
+++ BibliotecaDeClasses/BibliotecaDeClasses/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClasses
+{
+    public class Produto
+    {
+        private int Idprodutos = 1;
+        public string NomeProduto = "Cumbuca";
+        public double Preco = 15.99;
+        private int Quantidade = 5;
+    }
+}
