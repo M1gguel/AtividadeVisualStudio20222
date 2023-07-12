f7191784bdfa1eb8d3d7d7b557a8aa1b920558f2
M1gguel <134648203+M1gguel@users.noreply.github.com>
2023-07-12 17:20:58 -0300

    update 9.0

---
+++ ListadeComprasEdson/ListadeComprasEdson/ListadeCompras.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListadeComprasEdson
+{
+    internal class ListadeCompras
+    {
+        static public void  ImprimeLista(List <string> ListaDeItens, string Título = "--------------")
+        {
+            Título = "Cupom Fiscal";
+            Console.WriteLine("================= " + Título + "=================");
+
+            foreach (string iten in ListaDeItens) 
+            {
+                Console.WriteLine(iten);
+            }
+        }
+        static void Main()
+        {
+            List <string> ListaDeitens = new List<string>();
+            ListaDeitens.Add("1 - Arroz = 15.77");
+            ListaDeitens.Add("1 - Contra Filé = 45.99");
+            ListaDeitens.Add("1 - Batata = 17.47");
+
+            ListaDeitens.Sort();
+            ImprimeLista(ListaDeitens);
+
+            Console.WriteLine("==================================================");
+            Console.WriteLine("Total:                                         " + Total);
+        }
+    }
+}
